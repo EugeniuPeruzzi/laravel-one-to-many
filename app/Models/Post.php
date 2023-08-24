bd1cc65ae0103144c8ae96640229d03650fca4a6
EugeniuPeruzzi <jaka1997@live.it>
2023-08-24 17:04:06 +0200

    post belongs categroy hasmany

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,12 +6,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Support\Str;
+use App\Models\Category;
 
 class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'content', 'slug', 'image'];
+    protected $fillable = ['title', 'content', 'slug', 'image', 'category_id'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 
     // una funzione standarta che modifica il titpo da Ciao mondo a Ciao-mondo 
     public static function generateSlug($title){
