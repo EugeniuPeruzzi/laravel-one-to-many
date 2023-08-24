28548f844931006e2852f7b61f770432ab079d52
EugeniuPeruzzi <jaka1997@live.it>
2023-08-24 16:42:30 +0200

    added foreign key

--- database/migrations/2023_08_24_135708_create_categories_table.php
+++ database/migrations/2023_08_24_135708_create_categories_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 30);
+            $table->string('slug');
             $table->timestamps();
         });
     }
